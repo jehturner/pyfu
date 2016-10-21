6a2034fb48a4ab5d2afafc6b4764f5679284c69c
James Turner <jturner@gemini.edu>
2016-10-21 20:09:17 +0000

    Update version number for release.
    git-svn-id: http://scisoft.gemini.edu/svn/ifudr/pyfu/0.9@162 55eb1cfa-e442-4c38-9893-4da730078a3a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PyFU',
-      version='0.9dev',
+      version='0.9',
       description='Python scripts for mosaicking/resampling IFU data cubes',
       author='James E.H. Turner',
       author_email='jturner@gemini.edu',
